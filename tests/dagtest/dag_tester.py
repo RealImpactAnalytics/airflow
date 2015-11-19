2385d7cb9390b7748be30dae024685804e1bdfce
Thoralf Gutierrez <thoralf.gutierrez@realimpactanalytics.com>
2015-11-19 20:02:37 +0100

    create tmp output directory for each dag tests

--- tests/dagtest/dag_tester.py
+++ tests/dagtest/dag_tester.py
@@ -1,13 +1,15 @@
 """
     General entry point for testing end to end dags
 """
+import tempfile
 
 from airflow import configuration, AirflowException
 from airflow import executors, models, settings, utils
 from airflow.configuration import DEFAULT_CONFIG, AIRFLOW_HOME
-from airflow.models import DagBag
+from airflow.models import DagBag, Variable
 from airflow.settings import Session
 import os
+import logging
 import re
 
 
@@ -22,7 +24,7 @@ class DagBackfillTest(object):
     def get_dag_id(self):
         raise NotImplementedError()
 
-    def post_check(self):
+    def post_check(self, working_dir):
         raise NotImplementedError()
 
     def reset(self, dag_id):
@@ -37,6 +39,7 @@ class DagBackfillTest(object):
         # build a config file with a dag folder pointing to the tested dags
         config = configuration.default_config()
         config = re.sub("dags_folder =.*", "dags_folder = %s" % dags_folder, config)
+        config = re.sub("job_heartbeat_sec =.*", "job_heartbeat_sec = 1", config)
 
         # this is the config file that will be used by the child process
         config_location = "%s/dag_test_airflow.cfg" % AIRFLOW_HOME
@@ -48,10 +51,9 @@ class DagBackfillTest(object):
 
         return config_location
 
-
     def test_run(self):
 
-        #configuration.test_mode()
+        temp_dir = self.add_tmp_dir_variable()
         dags_folder = "%s/dags" % os.path.dirname(__file__)
         config_location = self.copy_config(dags_folder )
 
@@ -63,7 +65,7 @@ class DagBackfillTest(object):
         dag = dagbag.dags[self.get_dag_id()]
         job = self.build_job(dag)
 
-        # we must set the sequencial environment ourselves to control
+        # we must set the sequential environment ourselves to control
         if job.executor != executors.DEFAULT_EXECUTOR:
             raise AirflowException("DAG test may not set the executor")
 
@@ -76,5 +78,25 @@ class DagBackfillTest(object):
         job.dag.clear()
         job.run()
 
-        self.post_check()
+        self.post_check(temp_dir)
+
+        os.system("rm -rf %s" % temp_dir)
         self.reset(job.dag.dag_id)
+
+    def add_tmp_dir_variable(self):
+
+        unit_test_tmp_dir = tempfile.mkdtemp()
+
+        session = settings.Session()
+
+        old_var = session.query(Variable).filter_by(
+            key="unit_test_tmp_dir").first()
+
+        session.delete(old_var)
+        session.commit()
+
+        var = Variable(key="unit_test_tmp_dir", val=unit_test_tmp_dir)
+        session.add(var)
+        session.commit()
+
+        return unit_test_tmp_dir
