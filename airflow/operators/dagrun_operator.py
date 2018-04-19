f2edbabbbc97dbb332f764b974eaf3606b77d3b0
milanvdm <milanvdm@hotmail.com>
2018-04-19 09:14:23 +0200

    blah

--- airflow/operators/dagrun_operator.py
+++ airflow/operators/dagrun_operator.py
@@ -43,7 +43,7 @@ class TriggerDagRunOperator(BaseOperator):
         should look like ``def foo(context, dag_run_obj):``
     :type python_callable: python callable
     """
-    template_fields = ('execution_date')
+    template_fields = ('execution_date',)
     template_ext = tuple()
     ui_color = '#ffefeb'
 
